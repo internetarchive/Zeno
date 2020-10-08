e46f7049bd51d6804794bc15bc7102ce77ff3ddf
Corentin Barreau <corentin@archive.org>
2020-10-08 18:53:24 +0200

    Add active workers count to live stats

--- internal/pkg/crawl/stats.go
+++ internal/pkg/crawl/stats.go
@@ -44,6 +44,7 @@ func (c *Crawl) printLiveStats() {
 
 		stats.AddRow("", "")
 		stats.AddRow("  - Job:", c.Job)
+		stats.AddRow("  - Active workers:", c.ActiveWorkers.Value())
 		stats.AddRow("  - URL/s:", c.URLsPerSecond.Rate())
 		stats.AddRow("  - Crawled:", c.Crawled.Value())
 		stats.AddRow("  - Queued:", c.Frontier.QueueCount.Value())
