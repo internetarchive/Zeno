d80323f998ac322d5c6d38b07fd0982661bd2e12
Jake LaFountain <jakelf@archive.org>
2022-07-22 17:25:54 -0400

    feat: add data-srcset

--- internal/pkg/crawl/assets.go
+++ internal/pkg/crawl/assets.go
@@ -27,6 +27,14 @@ func (c *Crawl) extractAssets(base *url.URL, item *frontier.Item, doc *goquery.D
 				rawAssets = append(rawAssets, link)
 			}
 
+			link, exists = item.Attr("data-srcset")
+			if exists {
+				links := strings.Split(link, ",")
+				for _, link := range links {
+					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
+				}
+			}
+
 			link, exists = item.Attr("srcset")
 			if exists {
 				links := strings.Split(link, ",")
@@ -184,6 +192,14 @@ func (c *Crawl) extractAssets(base *url.URL, item *frontier.Item, doc *goquery.D
 			}
 
 			link, exists = item.Attr("srcset")
+			if exists {
+				links := strings.Split(link, ",")
+				for _, link := range links {
+					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
+				}
+			}
+
+			link, exists = item.Attr("data-srcset")
 			if exists {
 				links := strings.Split(link, ",")
 				for _, link := range links {
