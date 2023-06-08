df4c07913cff9334b30577707d3612e19831eb5e
Corentin Barreau <corentin@archive.org>
2023-06-08 12:53:22 +0200

    refactor: refactor logging and simplify GetOutboundIP in network module
    - Remove unused import of &#34;log&#34;
    - Replace log.Fatal with logrus.Errorf and add retry logic for network error
    - Simplify return statement for GetOutboundIP using method chaining
    
    Signed-off-by: Corentin Barreau <corentin@archive.org>

--- internal/pkg/utils/ip.go
+++ internal/pkg/utils/ip.go
@@ -1,20 +1,25 @@
 package utils
 
 import (
-	"log"
 	"net"
+
+	"github.com/sirupsen/logrus"
 )
 
 // Note: GetOutboundIP does not establish any connection and the
 // destination does not need to exist for this function to work.
 func GetOutboundIP() net.IP {
-	conn, err := net.Dial("udp", "24.24.24.24:24200")
-	if err != nil {
-		log.Fatal(err)
+	var conn net.Conn
+
+	for {
+		conn, err := net.Dial("udp", "24.24.24.24:24200")
+		if err != nil {
+			logrus.Errorf("error getting outbound IP, retrying: %s", err)
+			continue
+		}
+		defer conn.Close()
+		break
 	}
-	defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP
+	return conn.LocalAddr().(*net.UDPAddr).IP
 }
