5621854690f00111dbfc049b8003e0c5a421341e
Jake LaFountain <jakelf@archive.org>
2023-06-12 14:52:06 -0400

    Fix: panic from improper redefinition of net.Conn
    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0xbd8d78]
    
    goroutine 99 [running]:
    github.com/CorentinB/Zeno/internal/pkg/utils.GetOutboundIP()
            /tmp/Zeno/internal/pkg/utils/ip.go:24 +0xf8

--- internal/pkg/utils/ip.go
+++ internal/pkg/utils/ip.go
@@ -10,9 +10,10 @@ import (
 // destination does not need to exist for this function to work.
 func GetOutboundIP() net.IP {
 	var conn net.Conn
+	var err error
 
 	for {
-		conn, err := net.Dial("udp", "24.24.24.24:24200")
+		conn, err = net.Dial("udp", "24.24.24.24:24200")
 		if err != nil {
 			logrus.Errorf("error getting outbound IP, retrying: %s", err)
 			continue
