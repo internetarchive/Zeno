36421a6bf337eb0161bd39c169c8746e41231040
Jake L <NGTmeaty@users.noreply.github.com>
2023-10-18 08:19:50 +0000

    Fix: autoscroll

--- internal/pkg/crawl/headless.go
+++ internal/pkg/crawl/headless.go
@@ -123,43 +123,25 @@ func (c *Crawl) captureHeadless(item *frontier.Item) (respBody string, respHeade
 
 	// Scroll to the bottom of the page
 	js := `
-		() =>{ 
-			(function() {
-				var intervalObj = null;
-				var retry = 0;
-				var clickHandler = function() { 
-					console.log("Clicked; stopping autoscroll");
-					clearInterval(intervalObj);
-					document.body.removeEventListener("click", clickHandler);
-				}
-				function scrollDown() { 
-					var scrollHeight = document.body.scrollHeight,
-						scrollTop = document.body.scrollTop,
-						innerHeight = window.innerHeight,
-						difference = (scrollHeight - scrollTop) - innerHeight
-			
-					if (difference > 0) { 
-						window.scrollBy(0, difference);
-						if (retry > 0) { 
-							retry = 0;
-						}
-						console.log("scrolling down more");
-					} else {
-						if (retry >= 3) {
-							console.log("reached bottom of page; stopping");
-							clearInterval(intervalObj);
-							document.body.removeEventListener("click", clickHandler);
-						} else {
-							console.log("[apparenty] hit bottom of page; retrying: " + (retry + 1));
-							retry++;
-						}
-					}
-				}
-			
-				document.body.addEventListener("click", clickHandler);
-				intervalObj = setInterval(scrollDown, 1000);
-			})()
+	var intervalObj = null;
+	var clickHandler = function () {
+		console.log("Clicked; stopping autoscroll");
+		clearInterval(intervalObj);
+		document.body.removeEventListener("click", clickHandler);
+	}
+	function scrollDown() {
+		if (window.pageYOffset + window.innerHeight < document.body.scrollHeight - 1) {
+			window.scrollBy(0, window.innerHeight);
+			console.log("scrolling down more");
+		} else {
+			console.log("reached bottom of page; stopping");
+			clearInterval(intervalObj);
+			document.body.removeEventListener("click", clickHandler);
 		}
+	}
+
+	document.body.addEventListener("click", clickHandler);
+	intervalObj = setInterval(scrollDown, 500);
 	`
 
 	_, err = page.Timeout(c.Client.Timeout).Eval(js)
