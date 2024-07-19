d09be8e066ea21a13db60a34d6638130e766bf61
Thomas FOUBERT <perso@thomas-foubert.com>
2024-07-19 14:37:38 -0400

    chore: aligned Config struct type to save memory

--- config/config.go
+++ config/config.go
@@ -18,73 +18,76 @@ type Config struct {
 	LogLevel string `mapstructure:"log-level"`
 
 	// Get flags (crawling flags)
-	UserAgent                      string   `mapstructure:"user-agent"`
-	Job                            string   `mapstructure:"job"`
+	UserAgent        string `mapstructure:"user-agent"`
+	Job              string `mapstructure:"job"`
+	Cookies          string `mapstructure:"cookies"`
+	APIPort          string `mapstructure:"api-port"`
+	PrometheusPrefix string `mapstructure:"prometheus-prefix"`
+
+	// Get flags (Proxy flags)
+	Proxy string `mapstructure:"proxy"`
+
+	// Get flags (WARC flags)
+	WARCPrefix      string `mapstructure:"warc-prefix"`
+	WARCOperator    string `mapstructure:"warc-operator"`
+	CDXDedupeServer string `mapstructure:"warc-cdx-dedupe-server"`
+	WARCTempDir     string `mapstructure:"warc-temp-dir"`
+	CDXCookie       string `mapstructure:"cdx-cookie"`
+
+	HQAddress                string   `mapstructure:"hq-address"`
+	HQKey                    string   `mapstructure:"hq-key"`
+	HQSecret                 string   `mapstructure:"hq-secret"`
+	HQProject                string   `mapstructure:"hq-project"`
+	HQStrategy               string   `mapstructure:"hq-strategy"`
+	LogFileOutputDir         string   `mapstructure:"log-file-output-dir"`
+	ElasticSearchUsername    string   `mapstructure:"es-user"`
+	ElasticSearchPassword    string   `mapstructure:"es-password"`
+	ElasticSearchIndexPrefix string   `mapstructure:"es-index-prefix"`
+	DisableHTMLTag           []string `mapstructure:"disable-html-tag"`
+	ExcludeHosts             []string `mapstructure:"exclude-host"`
+	IncludeHosts             []string `mapstructure:"include-host"`
+	ExcludeString            []string `mapstructure:"exclude-string"`
+	DomainsBypassProxy       []string `mapstructure:"bypass-proxy"`
+
+	ElasticSearchURLs              []string `mapstructure:"es-url"`
 	WorkersCount                   int      `mapstructure:"workers"`
 	MaxConcurrentAssets            int      `mapstructure:"max-concurrent-assets"`
 	MaxHops                        uint     `mapstructure:"max-hops"`
-	Cookies                        string   `mapstructure:"cookies"`
-	KeepCookies                    bool     `mapstructure:"keep-cookies"`
-	Headless                       bool     `mapstructure:"headless"`
-	LocalSeencheck                 bool     `mapstructure:"local-seencheck"`
-	JSON                           bool     `mapstructure:"json"`
-	Debug                          bool     `mapstructure:"debug"`
-	LiveStats                      bool     `mapstructure:"live-stats"`
-	API                            bool     `mapstructure:"api"`
-	APIPort                        string   `mapstructure:"api-port"`
-	Prometheus                     bool     `mapstructure:"prometheus"`
-	PrometheusPrefix               string   `mapstructure:"prometheus-prefix"`
 	MaxRedirect                    int      `mapstructure:"max-redirect"`
 	MaxRetry                       int      `mapstructure:"max-retry"`
 	HTTPTimeout                    int      `mapstructure:"http-timeout"`
-	DomainsCrawl                   bool     `mapstructure:"domains-crawl"`
-	DisableHTMLTag                 []string `mapstructure:"disable-html-tag"`
-	CaptureAlternatePages          bool     `mapstructure:"capture-alternate-pages"`
-	ExcludeHosts                   []string `mapstructure:"exclude-host"`
-	IncludeHosts                   []string `mapstructure:"include-host"`
 	MaxConcurrentRequestsPerDomain int      `mapstructure:"max-concurrent-per-domain"`
 	ConcurrentSleepLength          int      `mapstructure:"concurrent-sleep-length"`
 	CrawlTimeLimit                 int      `mapstructure:"crawl-time-limit"`
 	CrawlMaxTimeLimit              int      `mapstructure:"crawl-max-time-limit"`
-	ExcludeString                  []string `mapstructure:"exclude-string"`
-	RandomLocalIP                  bool     `mapstructure:"random-local-ip"`
 	MinSpaceRequired               int      `mapstructure:"min-space-required"`
 
-	// Get flags (Proxy flags)
-	Proxy              string   `mapstructure:"proxy"`
-	DomainsBypassProxy []string `mapstructure:"bypass-proxy"`
-
-	// Get flags (WARC flags)
-	WARCPrefix           string `mapstructure:"warc-prefix"`
-	WARCOperator         string `mapstructure:"warc-operator"`
-	CDXDedupeServer      string `mapstructure:"warc-cdx-dedupe-server"`
-	WARCOnDisk           bool   `mapstructure:"warc-on-disk"`
-	WARCPoolSize         int    `mapstructure:"warc-pool-size"`
-	WARCTempDir          string `mapstructure:"warc-temp-dir"`
-	DisableLocalDedupe   bool   `mapstructure:"disable-local-dedupe"`
-	CertValidation       bool   `mapstructure:"cert-validation"`
-	DisableAssetsCapture bool   `mapstructure:"disable-assets-capture"`
-	WARCDedupeSize       int    `mapstructure:"warc-dedupe-size"`
-	CDXCookie            string `mapstructure:"cdx-cookie"`
+	WARCPoolSize          int   `mapstructure:"warc-pool-size"`
+	WARCDedupeSize        int   `mapstructure:"warc-dedupe-size"`
+	HQBatchSize           int64 `mapstructure:"hq-batch-size"`
+	KeepCookies           bool  `mapstructure:"keep-cookies"`
+	Headless              bool  `mapstructure:"headless"`
+	LocalSeencheck        bool  `mapstructure:"local-seencheck"`
+	JSON                  bool  `mapstructure:"json"`
+	Debug                 bool  `mapstructure:"debug"`
+	LiveStats             bool  `mapstructure:"live-stats"`
+	API                   bool  `mapstructure:"api"`
+	Prometheus            bool  `mapstructure:"prometheus"`
+	DomainsCrawl          bool  `mapstructure:"domains-crawl"`
+	CaptureAlternatePages bool  `mapstructure:"capture-alternate-pages"`
+	RandomLocalIP         bool  `mapstructure:"random-local-ip"`
+	WARCOnDisk            bool  `mapstructure:"warc-on-disk"`
+	DisableLocalDedupe    bool  `mapstructure:"disable-local-dedupe"`
+	CertValidation        bool  `mapstructure:"cert-validation"`
+	DisableAssetsCapture  bool  `mapstructure:"disable-assets-capture"`
 
 	// Get flags (Crawl HQ flags)
-	HQ                  bool   // Special field to check if HQ is enabled depending on the command called
-	HQAddress           string `mapstructure:"hq-address"`
-	HQKey               string `mapstructure:"hq-key"`
-	HQSecret            string `mapstructure:"hq-secret"`
-	HQProject           string `mapstructure:"hq-project"`
-	HQBatchSize         int64  `mapstructure:"hq-batch-size"`
-	HQContinuousPull    bool   `mapstructure:"hq-continuous-pull"`
-	HQStrategy          string `mapstructure:"hq-strategy"`
-	HQRateLimitSendBack bool   `mapstructure:"hq-rate-limiting-send-back"`
+	HQ                  bool // Special field to check if HQ is enabled depending on the command called
+	HQContinuousPull    bool `mapstructure:"hq-continuous-pull"`
+	HQRateLimitSendBack bool `mapstructure:"hq-rate-limiting-send-back"`
 
 	// Get flags (Logging flags)
-	NoStdoutLogging          bool     `mapstructure:"no-stdout-log"`
-	LogFileOutputDir         string   `mapstructure:"log-file-output-dir"`
-	ElasticSearchURLs        []string `mapstructure:"es-url"`
-	ElasticSearchUsername    string   `mapstructure:"es-user"`
-	ElasticSearchPassword    string   `mapstructure:"es-password"`
-	ElasticSearchIndexPrefix string   `mapstructure:"es-index-prefix"`
+	NoStdoutLogging bool `mapstructure:"no-stdout-log"`
 }
 
 var (
