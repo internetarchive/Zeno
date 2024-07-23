a39c0d5506d4921ef01b34980d6aafdfad4e3906
Thomas FOUBERT <perso@thomas-foubert.com>
2024-07-23 15:03:20 -0400

    add more tests and corrected truncate accordingly

--- internal/pkg/queue/index/wal.go
+++ internal/pkg/queue/index/wal.go
@@ -1,13 +1,22 @@
 package index
 
 import (
+	"encoding/gob"
 	"fmt"
 	"io"
+	"os"
 )
 
-func (im *IndexManager) writeToWAL(Op Operation, host, id string, position, size uint64) error {
+// unsafeWriteToWAL writes an entry to the WAL file. This method is not thread-safe and should be called after acquiring a lock.
+func (im *IndexManager) unsafeWriteToWAL(Op Operation, host, id string, position, size uint64) error {
 	// Write to WAL
-	entry := WALEntry{Op: Op, Host: host, BlobID: id, Position: position, Size: size}
+	entry := WALEntry{
+		Op:       Op,
+		Host:     host,
+		BlobID:   id,
+		Position: position,
+		Size:     size,
+	}
 	if err := im.walEncoder.Encode(entry); err != nil {
 		return fmt.Errorf("failed to write to WAL: %w", err)
 	}
@@ -18,7 +27,11 @@ func (im *IndexManager) writeToWAL(Op Operation, host, id string, position, size
 	return nil
 }
 
-func (im *IndexManager) replayWAL(entriesReplayed *int) error {
+// unsafeReplayWAL replays the WAL file. This method is not thread-safe and should be called after acquiring a lock.
+// If entriesReplayed is not nil, it will be set to the number of entries replayed.
+// If the WAL file is empty, this method returns ErrNoWALEntriesReplayed.
+// If more than 0 entries are replayed, you MUST call unsafeTruncateWAL after it to avoid encoding issues.
+func (im *IndexManager) unsafeReplayWAL(entriesReplayed *int) error {
 	_, err := im.walFile.Seek(0, 0)
 	if err != nil {
 		return fmt.Errorf("failed to seek to the beginning of WAL: %w", err)
@@ -50,8 +63,6 @@ func (im *IndexManager) replayWAL(entriesReplayed *int) error {
 		tempEntriesReplayed++
 	}
 
-	fmt.Printf("Replayed %d entries from WAL\n", tempEntriesReplayed)
-
 	if tempEntriesReplayed == 0 {
 		return ErrNoWALEntriesReplayed
 	}
@@ -62,18 +73,39 @@ func (im *IndexManager) replayWAL(entriesReplayed *int) error {
 	return nil
 }
 
-func (im *IndexManager) truncateWAL() error {
-	if err := im.walFile.Truncate(0); err != nil {
+// unsafeTruncateWAL truncates the WAL file. This method is not thread-safe and should be called after acquiring a lock.
+func (im *IndexManager) unsafeTruncateWAL() error {
+	// Save WAL file path
+	walPath := im.walFile.Name()
+
+	// Void the encoder/decoder
+	im.walEncoder = nil
+	im.walDecoder = nil
+
+	// Close the current WAL file
+	err := im.walFile.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close WAL file: %w", err)
+	}
+
+	// Reopen the file with O_TRUNC flag to truncate it
+	walFile, err := os.OpenFile(walPath, os.O_TRUNC|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
 		return fmt.Errorf("failed to truncate WAL file: %w", err)
 	}
-	_, err := im.walFile.Seek(0, 0)
-	if err != nil {
-		return fmt.Errorf("failed to seek WAL file: %w", err)
-	}
+
+	// Replace the old file descriptor with the new one
+	im.walFile = walFile
+
+	// Create a new encoder and decoder for the fresh WAL file
+	im.walEncoder = gob.NewEncoder(im.walFile)
+	im.walDecoder = gob.NewDecoder(im.walFile)
+
 	return nil
 }
 
-func (im *IndexManager) isWALEmpty() (bool, error) {
+// unsafeIsWALEmpty checks if the WAL file is empty. This method is not thread-safe and should be called after acquiring a lock.
+func (im *IndexManager) unsafeIsWALEmpty() (bool, error) {
 	stat, err := im.walFile.Stat()
 	if err != nil {
 		return false, fmt.Errorf("failed to get WAL file info: %w", err)
