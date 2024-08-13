2b105d5df5ed8bcc0008c5a2e89e36d444e433c6
Jake LaFountain <jakelf@archive.org>
2024-08-13 04:15:44 -0400

    feat: add additional test with fully encoded URL with path.

--- internal/pkg/utils/url_test.go
+++ internal/pkg/utils/url_test.go
@@ -44,6 +44,19 @@ func TestURLToStringUnicodetoIDNA(t *testing.T) {
 	}
 }
 
+func TestURLToStringWithPath(t *testing.T) {
+	u, err := url.Parse("http://παράδειγμα.δοκιμή/Αρχική_σελίδα")
+	if err != nil {
+		t.Fatalf("Error parsing URL: %v", err)
+	}
+
+	expected := "http://xn--hxajbheg2az3al.xn--jxalpdlp/%CE%91%CF%81%CF%87%CE%B9%CE%BA%CE%AE_%CF%83%CE%B5%CE%BB%CE%AF%CE%B4%CE%B1"
+	actual := URLToString(u)
+	if actual != expected {
+		t.Fatalf("Expected %s, got %s", expected, actual)
+	}
+}
+
 func TestURLToStringUnicodetoIDNAWithPort(t *testing.T) {
 	u, err := url.Parse("https://о-змладйвеклблнозеж.xn--p1ia:8080/pic/file/map_of_sarlat.pdf")
 	if err != nil {
