60aaf9adddf34b6b0b58370a87c671736b0dfe0a
Corentin Barreau <corentin.barreau24@gmail.com>
2024-12-09 12:05:12 +0100

    fix: adapt postprocessor for tree-based items

--- internal/pkg/postprocessor/extractor/m3u8.go
+++ internal/pkg/postprocessor/extractor/m3u8.go
@@ -1,31 +1,32 @@
 package extractor
 
 import (
-	"net/http"
-	"net/url"
-
 	"github.com/grafov/m3u8"
+	"github.com/internetarchive/Zeno/pkg/models"
 )
 
-func IsM3U8(resp *http.Response) bool {
-	return isContentType(resp.Header.Get("Content-Type"), "application/vnd.apple.mpegurl") ||
-		isContentType(resp.Header.Get("Content-Type"), "application/x-mpegURL")
+func IsM3U8(URL *models.URL) bool {
+	return isContentType(URL.GetResponse().Header.Get("Content-Type"), "application/vnd.apple.mpegurl") ||
+		isContentType(URL.GetResponse().Header.Get("Content-Type"), "application/x-mpegURL")
 }
 
-func M3U8(resp *http.Response) (URLs []*url.URL, err error) {
-	playlist, listType, err := m3u8.DecodeFrom(resp.Body, true)
+func M3U8(URL *models.URL) (assets []*models.URL, err error) {
+	defer URL.RewindBody()
+
+	var rawAssets ([]string)
+
+	playlist, listType, err := m3u8.DecodeFrom(URL.GetBody(), true)
 	if err != nil {
-		return URLs, err
+		return assets, err
 	}
 
-	var rawURLs []string
 	switch listType {
 	case m3u8.MEDIA:
 		mediapl := playlist.(*m3u8.MediaPlaylist)
 
 		for _, segment := range mediapl.Segments {
 			if segment != nil && segment.URI != "" {
-				rawURLs = append(rawURLs, segment.URI)
+				rawAssets = append(rawAssets, segment.URI)
 			}
 		}
 	case m3u8.MASTER:
@@ -34,29 +35,24 @@ func M3U8(resp *http.Response) (URLs []*url.URL, err error) {
 		for _, variant := range masterpl.Variants {
 			if variant != nil {
 				if variant.URI != "" {
-					rawURLs = append(rawURLs, variant.URI)
+					rawAssets = append(rawAssets, variant.URI)
 				}
 
 				for _, alt := range variant.Alternatives {
 					if alt != nil && alt.URI != "" {
-						rawURLs = append(rawURLs, alt.URI)
+						rawAssets = append(rawAssets, alt.URI)
 					}
 				}
 			}
 		}
 	}
 
-	baseURL := resp.Request.URL
-
-	for _, rawURL := range rawURLs {
-		URL, err := url.Parse(rawURL)
-		if err == nil {
-			if !URL.IsAbs() {
-				URL = baseURL.ResolveReference(URL)
-			}
-			URLs = append(URLs, URL)
-		}
+	for _, rawAsset := range rawAssets {
+		assets = append(assets, &models.URL{
+			Raw:  rawAsset,
+			Hops: URL.GetHops(),
+		})
 	}
 
-	return URLs, nil
+	return assets, nil
 }
