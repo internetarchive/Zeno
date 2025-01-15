fd6125faa3384ed15344d8b61d29a0caf481a2f4
Thomas FOUBERT <perso@thomas-foubert.com>
2025-01-15 10:03:34 +0100

    postprocess: renamed some variables and corrected forloop variables

--- internal/pkg/postprocessor/postprocess.go
+++ internal/pkg/postprocessor/postprocess.go
@@ -7,14 +7,16 @@ import (
 	"github.com/internetarchive/Zeno/pkg/models"
 )
 
-func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
+func postprocessItem(item *models.Item) []*models.Item {
 	logger := log.NewFieldedLogger(&log.Fields{
 		"component": "postprocessor.postprocess.postprocessItem",
 	})
 
+	outlinks := make([]*models.Item, 0)
+
 	if item.GetStatus() != models.ItemArchived {
 		logger.Debug("item not archived, skipping", "item_id", item.GetShortID())
-		return
+		return outlinks
 	}
 
 	// Verify if there is any redirection
@@ -24,7 +26,7 @@ func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
 		if item.GetURL().GetRedirects() >= config.Get().MaxRedirect {
 			logger.Warn("max redirects reached", "item_id", item.GetShortID())
 			item.SetStatus(models.ItemCompleted)
-			return
+			return outlinks
 		}
 
 		// Prepare the new item resulting from the redirection
@@ -41,7 +43,7 @@ func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
 			panic(err)
 		}
 
-		return
+		return outlinks
 	}
 
 	// Execute site-specific post-processing
@@ -70,11 +72,11 @@ func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
 	if item.IsChild() && item.GetURL().GetHops() > 1 {
 		logger.Debug("item is child and URL has more than one hop", "item_id", item.GetShortID())
 		item.SetStatus(models.ItemCompleted)
-		return
+		return outlinks
 	} else if config.Get().DisableAssetsCapture && !config.Get().DomainsCrawl {
 		logger.Debug("assets capture and domains crawl are disabled", "item_id", item.GetShortID())
 		item.SetStatus(models.ItemCompleted)
-		return
+		return outlinks
 	}
 
 	if item.GetURL().GetResponse() != nil && item.GetURL().GetResponse().StatusCode == 200 {
@@ -92,7 +94,8 @@ func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
 				}
 
 				item.SetStatus(models.ItemGotChildren)
-				err = item.AddChild(models.NewItem(uuid.New().String(), asset, "", false), item.GetStatus())
+				newChild := models.NewItem(uuid.New().String(), asset, "", false)
+				err = item.AddChild(newChild, models.ItemGotChildren)
 				if err != nil {
 					panic(err)
 				}
@@ -101,22 +104,23 @@ func postprocessItem(item, _ *models.Item) (outlinks []*models.Item) {
 
 		// Extract outlinks from the page
 		if (config.Get().DomainsCrawl || ((item.IsSeed() || item.IsRedirection()) && item.GetURL().GetHops() < config.Get().MaxHops)) && item.GetURL().GetBody() != nil {
-			links, err := extractOutlinks(item)
+			newOutlinks, err := extractOutlinks(item)
 			if err != nil {
 				logger.Error("unable to extract outlinks", "err", err.Error(), "item", item.GetShortID())
-				return
+				return outlinks
 			}
 
-			for _, link := range links {
-				if link == nil {
+			for i := range newOutlinks {
+				if newOutlinks[i] == nil {
 					logger.Warn("nil link", "item", item.GetShortID())
 					continue
 				}
 
-				outlinks = append(outlinks, models.NewItem(uuid.New().String(), link, item.GetURL().String(), true))
+				newOutlinkItem := models.NewItem(uuid.New().String(), newOutlinks[i], item.GetURL().String(), true)
+				outlinks = append(outlinks, newOutlinkItem)
 			}
 
-			logger.Debug("extracted outlinks", "item", item.GetShortID(), "count", len(links))
+			logger.Debug("extracted outlinks", "item", item.GetShortID(), "count", len(newOutlinks))
 		}
 	}
 
