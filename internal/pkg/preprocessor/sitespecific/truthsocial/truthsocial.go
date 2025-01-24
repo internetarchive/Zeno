ce5b62c3ebc16a9f38b7bac1d295edc4500f87a4
Corentin Barreau <corentin@archive.org>
2025-01-24 12:25:08 +0100

    add: truthsocial.com preprocessing & post-processing

---
+++ internal/pkg/preprocessor/sitespecific/truthsocial/truthsocial.go
@@ -0,0 +1,27 @@
+package truthsocial
+
+import (
+	"net/http"
+	"regexp"
+
+	"github.com/internetarchive/Zeno/pkg/models"
+)
+
+var APIStatusRegex = regexp.MustCompile(`https:\/\/truthsocial\.com\/api\/v1\/statuses\/(\d+)`)
+
+func IsStatusAPIURL(URL *models.URL) bool {
+	return APIStatusRegex.MatchString(URL.String())
+}
+
+func AddStatusAPIHeaders(req *http.Request) {
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br, zstd")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-Site", "none")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Connection", "keep-alive")
+}
