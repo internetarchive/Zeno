898efa634786a094edd7df381e488b232e139a95
yzqzss <yzqzss@yandex.com>
2025-03-24 11:42:24 +0800

    fix: Zeno v2 doesn't report to HQ via websocket
    close: #198

--- internal/pkg/source/hq/websocket.go
+++ internal/pkg/source/hq/websocket.go
@@ -1,42 +1,56 @@
 package hq
 
 import (
-	"log/slog"
 	"time"
 
 	"github.com/internetarchive/Zeno/internal/pkg/config"
+	"github.com/internetarchive/Zeno/internal/pkg/log"
 	"github.com/internetarchive/Zeno/internal/pkg/utils"
 	"github.com/internetarchive/gocrawlhq"
 )
 
-// Websocket connects to HQ's websocket and listen for messages.
+// websocket connects to HQ's websocket and listen for messages.
 // It also sends and "identify" message to the HQ to let it know that
 // Zeno is connected. This "identify" message is sent every second and
 // contains the crawler's stats and details.
-func Websocket() {
-	var identifyTicker = time.NewTicker(time.Second)
+func websocket() {
+	logger := log.NewFieldedLogger(&log.Fields{
+		"component": "hq.websocket",
+	})
 
-	defer func() {
-		identifyTicker.Stop()
-	}()
+	identifyTicker := time.NewTicker(time.Second)
+	defer identifyTicker.Stop()
 
 	for {
-		err := globalHQ.client.Identify(&gocrawlhq.IdentifyMessage{
-			Project:   config.Get().HQProject,
-			Job:       config.Get().Job,
-			IP:        utils.GetOutboundIP().String(),
-			Hostname:  utils.GetHostname(),
-			GoVersion: utils.GetVersion().GoVersion,
-		})
-		if err != nil {
-			slog.Error("error sending identify payload to Crawl HQ, trying to reconnect", "err", err.Error())
-
-			err = globalHQ.client.InitWebsocketConn()
-			if err != nil {
-				slog.Error("error initializing websocket connection to crawl HQ", "err", err.Error())
-			}
+		select {
+		case <-globalHQ.ctx.Done():
+			logger.Debug("received done signal")
+			globalHQ.wg.Done()
+			return
+		default:
+			sendIdentify(logger)
+			<-identifyTicker.C
 		}
+	}
+}
 
-		<-identifyTicker.C
+func sendIdentify(logger *log.FieldedLogger) {
+	err := globalHQ.client.Identify(&gocrawlhq.IdentifyMessage{
+		Project:   config.Get().HQProject,
+		Job:       config.Get().Job,
+		IP:        utils.GetOutboundIP().String(),
+		Hostname:  utils.GetHostname(),
+		GoVersion: utils.GetVersion().GoVersion,
+	})
+	if err != nil {
+		logger.Error("error sending identify payload to Crawl HQ, trying to reconnect", "err", err.Error())
+		reconnectWebsocket(logger)
+	}
+}
+
+func reconnectWebsocket(logger *log.FieldedLogger) {
+	err := globalHQ.client.InitWebsocketConn()
+	if err != nil {
+		logger.Error("error initializing websocket connection to Crawl HQ", "err", err.Error())
 	}
 }
