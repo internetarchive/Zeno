abc1ecd08c095edf5183625a584ad32659af883c
Akshith <garapatiakshith@gmail.com>
2025-04-07 15:21:57 -0400

    feat: non utf-8 html support

--- internal/pkg/postprocessor/extractor/html.go
+++ internal/pkg/postprocessor/extractor/html.go
@@ -42,7 +42,6 @@ func HTMLOutlinks(item *models.Item) (outlinks []*models.URL, err error) {
 	extractBaseTag(item, document)
 
 	// Match <a> tags with href, data-href, data-src, data-srcset, data-lazy-src, data-srcset, src, srcset
-	// Extract potential URLs from <a> tags using common attributes
 	if !slices.Contains(config.Get().DisableHTMLTag, "a") {
 		attrs := []string{
 			"href",
@@ -81,22 +80,26 @@ func HTMLOutlinks(item *models.Item) (outlinks []*models.URL, err error) {
 		resolvedURL, err := resolveURL(rawOutlink, item)
 		if err != nil {
 			logger.Debug("unable to resolve URL", "error", err, "url", item.GetURL().String(), "item", item.GetShortID())
-		} else if resolvedURL != "" {
+			if rawOutlink == item.GetBase() || rawOutlink == item.GetURL().String() {
+				logger.Debug("discarding outlink because it is the same as the base URL or current URL", "url", rawOutlink, "item", item.GetShortID())
+				continue
+			}
 			outlinks = append(outlinks, &models.URL{
-				Raw: resolvedURL,
+				Raw: rawOutlink,
 			})
-			continue
+		} else if resolvedURL != "" {
+			normalizedURL, err := item.GetURL().NormalizeURL(resolvedURL)
+			if err != nil {
+				logger.Debug("unable to normalize URL", "error", err, "url", resolvedURL, "item", item.GetShortID())
+				outlinks = append(outlinks, &models.URL{
+					Raw: resolvedURL,
+				})
+			} else {
+				outlinks = append(outlinks, &models.URL{
+					Raw: normalizedURL,
+				})
+			}
 		}
-
-		// Discard URLs that are the same as the base URL or the current URL
-		if rawOutlink == item.GetBase() || rawOutlink == item.GetURL().String() {
-			logger.Debug("discarding outlink because it is the same as the base URL or current URL", "url", rawOutlink, "item", item.GetShortID())
-			continue
-		}
-
-		outlinks = append(outlinks, &models.URL{
-			Raw: rawOutlink,
-		})
 	}
 
 	return outlinks, nil
@@ -107,7 +110,27 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 		"component": "postprocessor.extractor.HTMLAssets",
 	})
 
-	var rawAssets []string
+	rawAssetsMap := make(map[string]bool)
+	// Helper function to add to map instead of directly to slice
+	addRawAsset := func(url string) {
+		if url == "" {
+			return
+		}
+
+		// Don't extract CSS elements that aren't URLs
+		if strings.Contains(url, "%") ||
+			strings.HasPrefix(url, "0.") ||
+			strings.HasPrefix(url, "--font") ||
+			strings.HasPrefix(url, "--size") ||
+			strings.HasPrefix(url, "--color") ||
+			strings.HasPrefix(url, "--shreddit") ||
+			strings.HasPrefix(url, "100vh") ||
+			strings.HasPrefix(url, "#wp-") {
+			return
+		}
+
+		rawAssetsMap[url] = true
+	}
 
 	// Retrieve (potentially creates it) the document from the body
 	document, err := item.GetURL().GetDocument()
@@ -127,7 +150,9 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 			if err != nil {
 				logger.Debug("unable to extract URLs from JSON in data-item attribute", "err", err, "url", item.GetURL().String(), "item", item.GetShortID())
 			} else {
-				rawAssets = append(rawAssets, URLsFromJSON...)
+				for _, url := range URLsFromJSON {
+					addRawAsset(url)
+				}
 			}
 		}
 
@@ -138,19 +163,7 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 			for match := range matches {
 				if len(matches[match]) > 0 {
 					matchFound := matches[match][1]
-
-					// Don't extract CSS elements that aren't URLs
-					if strings.Contains(matchFound, "%") ||
-						strings.HasPrefix(matchFound, "0.") ||
-						strings.HasPrefix(matchFound, "--font") ||
-						strings.HasPrefix(matchFound, "--size") ||
-						strings.HasPrefix(matchFound, "--color") ||
-						strings.HasPrefix(matchFound, "--shreddit") ||
-						strings.HasPrefix(matchFound, "100vh") {
-						continue
-					}
-
-					rawAssets = append(rawAssets, matchFound)
+					addRawAsset(matchFound)
 				}
 			}
 		}
@@ -158,31 +171,20 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 		dataPreview, exists := i.Attr("data-preview")
 		if exists {
 			if strings.HasPrefix(dataPreview, "http") {
-				rawAssets = append(rawAssets, dataPreview)
+				addRawAsset(dataPreview)
 			}
 		}
 	})
 
-	// Try to find assets in <a> tags.. this is a bit funky
+	// Try to find assets in <a> tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "a") {
 		var validAssetPath = []string{
-			"static/",
-			"assets/",
-			"asset/",
-			"images/",
-			"image/",
-			"img/",
+			"static/", "assets/", "asset/", "images/", "image/", "img/",
 		}
 
 		var validAssetAttributes = []string{
-			"href",
-			"data-href",
-			"data-src",
-			"data-srcset",
-			"data-lazy-src",
-			"data-srcset",
-			"src",
-			"srcset",
+			"href", "data-href", "data-src", "data-srcset",
+			"data-lazy-src", "data-srcset", "src", "srcset",
 		}
 
 		document.Find("a").Each(func(index int, i *goquery.Selection) {
@@ -190,49 +192,36 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 				link, exists := i.Attr(attr)
 				if exists {
 					if utils.StringContainsSliceElements(link, validAssetPath) {
-						rawAssets = append(rawAssets, link)
+						addRawAsset(link)
 					}
 				}
 			}
 		})
 	}
 
-	// Extract assets on the page (images, scripts, videos..)
+	// Handle <img> tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "img") {
 		document.Find("img").Each(func(index int, i *goquery.Selection) {
-			link, exists := i.Attr("src")
-			if exists {
-				rawAssets = append(rawAssets, link)
-			}
-
-			link, exists = i.Attr("data-src")
-			if exists {
-				rawAssets = append(rawAssets, link)
-			}
-
-			link, exists = i.Attr("data-lazy-src")
-			if exists {
-				rawAssets = append(rawAssets, link)
-			}
-
-			link, exists = i.Attr("data-srcset")
-			if exists {
-				links := strings.Split(link, ",")
-				for _, link := range links {
-					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
+			// Handle various image attributes
+			for _, attr := range []string{"src", "data-src", "data-lazy-src"} {
+				if link, exists := i.Attr(attr); exists {
+					addRawAsset(link)
 				}
 			}
 
-			link, exists = i.Attr("srcset")
-			if exists {
-				links := strings.Split(link, ",")
-				for _, link := range links {
-					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
+			// Handle srcset and data-srcset attributes
+			for _, srcsetAttr := range []string{"srcset", "data-srcset"} {
+				if link, exists := i.Attr(srcsetAttr); exists {
+					links := strings.Split(link, ",")
+					for _, link := range links {
+						addRawAsset(strings.Split(strings.TrimSpace(link), " ")[0])
+					}
 				}
 			}
 		})
 	}
 
+	// Handle video and audio tags
 	var targetElements = []string{}
 	if !slices.Contains(config.Get().DisableHTMLTag, "video") {
 		targetElements = append(targetElements, "video[src]")
@@ -243,11 +232,12 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 	if len(targetElements) > 0 {
 		document.Find(strings.Join(targetElements, ", ")).Each(func(index int, i *goquery.Selection) {
 			if link, exists := i.Attr("src"); exists {
-				rawAssets = append(rawAssets, link)
+				addRawAsset(link)
 			}
 		})
 	}
 
+	// Handle style tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "style") {
 		document.Find("style").Each(func(index int, i *goquery.Selection) {
 			matches := urlRegex.FindAllStringSubmatch(i.Text(), -1)
@@ -255,72 +245,59 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 				matchReplacement := matches[match][1]
 				matchReplacement = strings.Replace(matchReplacement, "'", "", -1)
 				matchReplacement = strings.Replace(matchReplacement, "\"", "", -1)
-
-				// If the URL already has http (or https), we don't need add anything to it.
 				if !strings.Contains(matchReplacement, "http") {
 					matchReplacement = strings.Replace(matchReplacement, "//", "http://", -1)
 				}
-
-				if strings.HasPrefix(matchReplacement, "#wp-") {
-					continue
-				}
-
-				rawAssets = append(rawAssets, matchReplacement)
+				addRawAsset(matchReplacement)
 			}
 		})
 	}
 
+	// Handle script tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "script") {
 		document.Find("script").Each(func(index int, i *goquery.Selection) {
-			link, exists := i.Attr("src")
-			if exists {
-				rawAssets = append(rawAssets, link)
+			if link, exists := i.Attr("src"); exists {
+				addRawAsset(link)
 			}
 
 			scriptType, exists := i.Attr("type")
-			if exists {
-				if strings.Contains(scriptType, "json") {
-					URLsFromJSON, _, err := GetURLsFromJSON(json.NewDecoder(strings.NewReader(i.Text())))
-					if err != nil {
-						// TODO: maybe add back when https://github.com/internetarchive/Zeno/issues/147 is fixed
-						// c.Log.Debug("unable to extract URLs from JSON in script tag", "error", err, "url", URL)
-					} else {
-						rawAssets = append(rawAssets, URLsFromJSON...)
+			if exists && scriptType == "application/json" {
+				URLsFromJSON, _, err := GetURLsFromJSON(json.NewDecoder(strings.NewReader(i.Text())))
+				if err == nil {
+					for _, url := range URLsFromJSON {
+						addRawAsset(url)
 					}
 				}
 			}
 
 			// Apply regex on the script's HTML to extract potential assets
 			outerHTML, err := goquery.OuterHtml(i)
-			if err != nil {
-				logger.Debug("unable to extract outer HTML from script tag", "err", err, "url", item.GetURL().String(), "item", item.GetShortID())
-			} else {
+			if err == nil {
 				scriptLinks := utils.DedupeStrings(LinkRegexStrict.FindAllString(outerHTML, -1))
 				for _, scriptLink := range scriptLinks {
 					if strings.HasPrefix(scriptLink, "http") {
 						// Escape URLs when unicode runes are present in the extracted URLs
-						scriptLink, err := strconv.Unquote(`"` + scriptLink + `"`)
-						if err != nil {
-							logger.Debug("unable to escape URL from JSON in script tag", "error", err, "url", item.GetURL().String(), "item", item.GetShortID())
-							continue
+						escapedLink, err := strconv.Unquote(`"` + scriptLink + `"`)
+						if err == nil {
+							addRawAsset(escapedLink)
 						}
-						rawAssets = append(rawAssets, scriptLink)
 					}
 				}
 			}
 
-			// Some <script> embed variable initialisation, we can strip the variable part and just scrape JSON
+			// Handle variable initialization in scripts
 			if !strings.HasPrefix(i.Text(), "{") {
 				assetsFromScriptContent, err := extractFromScriptContent(i.Text())
-				if err != nil {
-					logger.Debug("unable to extract URLs from JSON in script tag", "error", err, "url", item.GetURL().String(), "item", item.GetShortID())
-				} else {
-					rawAssets = append(rawAssets, assetsFromScriptContent...)
+				if err == nil {
+					for _, url := range assetsFromScriptContent {
+						addRawAsset(url)
+					}
 				}
 			}
 		})
 	}
 
+	// Handle link tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "link") {
 		document.Find("link").Each(func(index int, i *goquery.Selection) {
 			if !config.Get().CaptureAlternatePages {
@@ -330,58 +307,57 @@ func HTMLAssets(item *models.Item) (assets []*models.URL, err error) {
 				}
 			}
 
-			link, exists := i.Attr("href")
-			if exists {
-				rawAssets = append(rawAssets, link)
+			if link, exists := i.Attr("href"); exists {
+				addRawAsset(link)
 			}
 		})
 	}
 
+	// Handle meta tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "meta") {
 		document.Find("meta").Each(func(index int, i *goquery.Selection) {
-			link, exists := i.Attr("href")
-			if exists {
-				rawAssets = append(rawAssets, link)
+			if link, exists := i.Attr("href"); exists {
+				addRawAsset(link)
 			}
-			link, exists = i.Attr("content")
-			if exists {
-				if strings.Contains(link, "http") {
-					rawAssets = append(rawAssets, link)
-				}
+
+			if link, exists := i.Attr("content"); exists && strings.Contains(link, "http") {
+				addRawAsset(link)
 			}
 		})
 	}
 
+	// Handle source tags
 	if !slices.Contains(config.Get().DisableHTMLTag, "source") {
 		document.Find("source").Each(func(index int, i *goquery.Selection) {
-			link, exists := i.Attr("src")
-			if exists {
-				rawAssets = append(rawAssets, link)
+			if link, exists := i.Attr("src"); exists {
+				addRawAsset(link)
 			}
 
-			link, exists = i.Attr("srcset")
-			if exists {
-				links := strings.Split(link, ",")
-				for _, link := range links {
-					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
-				}
-			}
-
-			link, exists = i.Attr("data-srcset")
-			if exists {
-				links := strings.Split(link, ",")
-				for _, link := range links {
-					rawAssets = append(rawAssets, strings.Split(strings.TrimSpace(link), " ")[0])
+			// Handle srcset and data-srcset attributes
+			for _, srcsetAttr := range []string{"srcset", "data-srcset"} {
+				if link, exists := i.Attr(srcsetAttr); exists {
+					links := strings.Split(link, ",")
+					for _, link := range links {
+						addRawAsset(strings.Split(strings.TrimSpace(link), " ")[0])
+					}
 				}
 			}
 		})
 	}
 
-	for _, rawAsset := range rawAssets {
+	var uniqueRawAssets []string
+	for asset := range rawAssetsMap {
+		uniqueRawAssets = append(uniqueRawAssets, asset)
+	}
+	// Create model URLs from unique raw assets
+	for _, rawAsset := range uniqueRawAssets {
+		normalizedURL, err := item.GetURL().NormalizeURL(rawAsset)
+		if err != nil {
+			normalizedURL = rawAsset
+		}
 		assets = append(assets, &models.URL{
-			Raw: rawAsset,
+			Raw: normalizedURL,
 		})
-
 	}
 
 	return assets, nil
--- pkg/models/url.go
+++ pkg/models/url.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"log/slog"
 	"net"
@@ -12,7 +15,10 @@ import (
 	"github.com/CorentinB/warc/pkg/spooledtempfile"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gabriel-vasile/mimetype"
+	"golang.org/x/net/html/charset"
 	"golang.org/x/net/idna"
+	"golang.org/x/text/encoding/htmlindex"
+	"golang.org/x/text/transform"
 )
 
 type URL struct {
@@ -28,6 +34,7 @@ type URL struct {
 
 	stringCache string
 	once        sync.Once
+	documentMu  sync.Mutex // Protect document parsing
 }
 
 func (u *URL) Parse() (err error) {
@@ -43,17 +50,254 @@ func (u *URL) SetBody(body spooledtempfile.ReadSeekCloser) {
 	u.body = body
 }
 
+func (u *URL) NormalizeURL(rawURL string) (string, error) {
+	if rawURL == "" {
+		return rawURL, nil
+	}
+
+	// Handle special cases like data: and javascript: URLs
+	if strings.HasPrefix(rawURL, "data:") || strings.HasPrefix(rawURL, "javascript:") {
+		return rawURL, nil
+	}
+
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		// Try to handle partially encoded URLs
+		decodedURL := rawURL
+
+		// Attempt to safely decode any percent-encoded sequences
+		// if possible in case of multiple encodings
+		// (this should be uncommon and is defensive)
+		for attempt := 0; attempt < 3; attempt++ {
+			prev := decodedURL
+			tmpDecoded, tmpErr := url.QueryUnescape(decodedURL)
+			if tmpErr != nil {
+				break
+			}
+			decodedURL = tmpDecoded
+			if decodedURL == prev {
+				break
+			}
+		}
+
+		// Try parsing again with our best attempt at decoding.
+		// If all else fails, return the original URL.
+		parsedURL, err = url.Parse(decodedURL)
+		if err != nil {
+			return rawURL, err
+		}
+	}
+
+	var normalizedURL strings.Builder
+	if parsedURL.Scheme != "" {
+		normalizedURL.WriteString(parsedURL.Scheme)
+		normalizedURL.WriteString("://")
+	}
+
+	if parsedURL.Host != "" {
+		// For IDN domains, make sure we're using the Unicode representation
+		host := parsedURL.Host
+		if strings.HasPrefix(host, "xn--") {
+			ascii, err := idna.ToUnicode(host)
+			if err == nil {
+				host = ascii
+			}
+		}
+		normalizedURL.WriteString(host)
+	}
+
+	// Add path (properly decoded to preserve Unicode)
+	if parsedURL.Path != "" {
+		if parsedURL.Host != "" && !strings.HasPrefix(parsedURL.Path, "/") {
+			normalizedURL.WriteString("/")
+		}
+		decodedPath, err := url.PathUnescape(parsedURL.Path)
+		if err == nil {
+			normalizedURL.WriteString(decodedPath)
+		} else {
+			normalizedURL.WriteString(parsedURL.Path)
+		}
+	}
+
+	// Add query and then fragment
+	if parsedURL.RawQuery != "" {
+		normalizedURL.WriteString("?")
+		normalizedURL.WriteString(parsedURL.RawQuery)
+	}
+
+	if parsedURL.Fragment != "" {
+		normalizedURL.WriteString("#")
+		normalizedURL.WriteString(parsedURL.Fragment)
+	}
+
+	return normalizedURL.String(), nil
+}
+
+func createDecodedReader(body io.Reader, contentType string) (io.Reader, error) {
+	if seeker, ok := body.(io.Seeker); ok {
+		_, err := seeker.Seek(0, io.SeekStart)
+		if err != nil {
+			return nil, fmt.Errorf("failed to rewind body: %w", err)
+		}
+	}
+
+	bufferedReader := bufio.NewReader(body)
+
+	// Detect charset from content by peeking
+	var detectedCharset string
+	if data, err := bufferedReader.Peek(1024); err == nil {
+		if _, name, ok := charset.DetermineEncoding(data, contentType); ok {
+			detectedCharset = name
+			slog.Debug("Detected charset", "charset", detectedCharset, "content-type", contentType)
+		}
+	}
+
+	// If no charset detected, try from content-type header
+	if detectedCharset == "" {
+		detectedCharset = getCharsetFromContentType(contentType)
+	}
+
+	// If still no charset, default to UTF-8
+	if detectedCharset == "" {
+		detectedCharset = "utf-8"
+	}
+
+	// If detected charset is UTF-8, return buffered reader with no changes
+	if strings.EqualFold(detectedCharset, "utf-8") {
+		return bufferedReader, nil
+	}
+
+	e, err := htmlindex.Get(detectedCharset)
+	if err != nil {
+		// If we can't get encoding, default to UTF-8 and return the original body which is already
+		// valid UTF-8.
+		slog.Warn("Unknown charset", "charset", detectedCharset, "error", err)
+		return body, nil
+	}
+	decoder := e.NewDecoder()
+	return transform.NewReader(bufferedReader, decoder), nil
+}
+
+func getCharsetFromContentType(contentType string) string {
+	if contentType == "" {
+		return ""
+	}
+	for _, param := range strings.Split(contentType, ";") {
+		param = strings.TrimSpace(param)
+		if strings.HasPrefix(param, "charset=") {
+			return strings.TrimPrefix(param, "charset=")
+		}
+	}
+	return ""
+}
+
 func (u *URL) GetDocument() (doc *goquery.Document, err error) {
+	u.documentMu.Lock()
+	defer u.documentMu.Unlock()
+
 	if u.document == nil {
-		u.document, err = goquery.NewDocumentFromReader(u.GetBody())
+		resp := u.GetResponse()
+		if resp == nil {
+			return nil, fmt.Errorf("response is nil for URL: %s", u.Raw)
+		}
+		err = u.RewindBody()
+		if err != nil {
+			return nil, err
+		}
+		contentType := resp.Header.Get("Content-Type")
+
+		// Use the determineCharset function. The ignored value can be used
+		// to debug the determined charset.
+		_, bodyReader, err := u.determineCharset(contentType)
+		if err != nil {
+			slog.Warn("failed to determine charset, proceeding with original body",
+				"error", err,
+				"url", u.Raw,
+				"content-type", contentType)
+			err = u.RewindBody()
+			if err != nil {
+				return nil, err
+			}
+			bodyReader = u.GetBody()
+		}
+
+		// Create the document from the properly decoded reader
+		u.document, err = goquery.NewDocumentFromReader(bodyReader)
 		if err != nil {
 			return nil, err
 		}
 
-		u.RewindBody()
+		// Rewind body again after parsing
+		err = u.RewindBody()
+		if err != nil {
+			return nil, fmt.Errorf("could not rewind body after parsing: %w", err)
+		}
 	}
 
 	return u.document, nil
+}
+
+func (u *URL) determineCharset(contentType string) (string, io.Reader, error) {
+	bodyBytes, err := io.ReadAll(u.GetBody())
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Parse HTML to find meta tags first
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(bodyBytes))
+	if err != nil {
+		// If parsing fails, fall back to header (or utf-8)
+		return u.determineCharsetFallback(contentType, bodyBytes)
+	}
+
+	var metaCharset string
+	doc.Find("meta").Each(func(i int, s *goquery.Selection) {
+		charsetVal, exists := s.Attr("charset")
+		if exists {
+			metaCharset = charsetVal
+			return
+		}
+
+		httpEquiv, httpExists := s.Attr("http-equiv")
+		content, contentExists := s.Attr("content")
+
+		if httpExists && strings.ToLower(httpEquiv) == "content-type" && contentExists {
+			if strings.Contains(content, "charset=") {
+				metaCharset = strings.TrimSpace(strings.TrimPrefix(content, "text/html; charset="))
+				return
+			}
+		}
+	})
+
+	if metaCharset != "" {
+		decodedReader, err := createDecodedReader(bytes.NewReader(bodyBytes), "text/html; charset="+metaCharset)
+		if err != nil {
+			return "", bytes.NewReader(bodyBytes), err
+		}
+		return metaCharset, decodedReader, nil
+	}
+
+	// If no meta charset found, fall back to header (or UTF-8)
+	return u.determineCharsetFallback(contentType, bodyBytes)
+
+}
+
+func (u *URL) determineCharsetFallback(contentType string, bodyBytes []byte) (string, io.Reader, error) {
+	headerCharset := getCharsetFromContentType(contentType)
+	if headerCharset != "" {
+		decodedReader, err := createDecodedReader(bytes.NewReader(bodyBytes), contentType)
+		if err != nil {
+			return "", bytes.NewReader(bodyBytes), err
+		}
+		return headerCharset, decodedReader, nil
+	}
+
+	// If no header charset found, use createDecodedReader with UTF-8 as the fallback.
+	decodedReader, err := createDecodedReader(bytes.NewReader(bodyBytes), "text/html; charset=utf-8")
+	if err != nil {
+		return "", bytes.NewReader(bodyBytes), err
+	}
+	return "utf-8", decodedReader, nil
 }
 
 func (u *URL) SetDocument(doc *goquery.Document) {
@@ -68,11 +312,15 @@ func (u *URL) SetMIMEType(mimetype *mimetype.MIME) {
 	u.mimetype = mimetype
 }
 
-func (u *URL) RewindBody() {
+func (u *URL) RewindBody() error {
+	if u.body == nil {
+		return fmt.Errorf("body is nil")
+	}
 	_, err := u.body.Seek(0, io.SeekStart)
 	if err != nil {
-		panic(err)
+		slog.Warn("failed to rewind body", "error", err)
 	}
+	return err
 }
 
 func (u *URL) SetRequest(r *http.Request) {
@@ -121,36 +369,27 @@ func (u *URL) String() string {
 // URLToString exists to apply some custom stuff, in opposition of simply
 // using the u.parsed.String() method
 func URLToString(URL *url.URL) string {
-	var err error
 
-	switch URL.Host {
+	// Clone the URL to avoid modifying the original
+	urlCopy := *URL
+
+	switch urlCopy.Host {
 	case "external-preview.redd.it", "styles.redditmedia.com", "preview.redd.it":
 		// Do nothing. We don't want to encode the URL for signature purposes. :(
 		break
 	default:
-		URL.RawQuery = encodeQuery(URL.Query())
-	}
-
-	URL.Host, err = idna.ToASCII(URL.Host)
-	if err != nil {
-		if strings.Contains(URL.Host, ":") {
-			hostWithoutPort, port, err := net.SplitHostPort(URL.Host)
+		urlCopy.RawQuery = encodeQuery(urlCopy.Query())
+		if strings.Contains(urlCopy.Host, ":") {
+			hostWithoutPort, port, err := net.SplitHostPort(urlCopy.Host)
 			if err != nil {
 				slog.Warn("cannot split host and port", "error", err)
 			} else {
-				hostWithoutPort, err = idna.ToASCII(hostWithoutPort)
-				if err == nil {
-					URL.Host = hostWithoutPort + ":" + port
-				} else {
-					slog.Warn("cannot encode punycode host without port to ASCII", "error", err)
-				}
+				urlCopy.Host = hostWithoutPort + ":" + port
 			}
-		} else {
-			slog.Warn("cannot encode punycode host to ASCII", "error", err)
 		}
 	}
 
-	return URL.String()
+	return urlCopy.String()
 }
 
 // Encode encodes the values into “URL encoded” form
