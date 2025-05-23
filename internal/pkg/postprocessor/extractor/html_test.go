4b34763843a7654fa6367e79ec90abb5a7f3fe45
Vangelis Banos <vbanos@gmail.com>
2025-05-24 01:42:36 +0300

    Refactor HTML unit tests and increase coverage (#299)
    * Simplify HTML tests
    
    Create utility function to avoid duplicate code.
    
    * Add unit tests to cover more HTML assets/outlinks extraction

--- internal/pkg/postprocessor/extractor/html_test.go
+++ internal/pkg/postprocessor/extractor/html_test.go
@@ -12,9 +12,25 @@ import (
 	"github.com/internetarchive/Zeno/pkg/models"
 )
 
-func TestHTMLOutlinks(t *testing.T) {
+func TestMain(m *testing.M) {
 	config.InitConfig()
-	body := `
+	os.Exit(m.Run())
+}
+
+func setupItem(html string) *models.Item {
+	resp := &http.Response{
+		Body: io.NopCloser(bytes.NewBufferString(html)),
+	}
+	newURL := &models.URL{Raw: "http://ex.com"}
+	newURL.SetResponse(resp)
+	if err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir()); err != nil {
+		panic(err)
+	}
+	return models.NewItem("test", newURL, "")
+}
+
+func TestHTMLOutlinks(t *testing.T) {
+	html := `
 	<html>
 		<head></head>
 		<body>
@@ -24,19 +40,8 @@ func TestHTMLOutlinks(t *testing.T) {
 			<a href="https://web.archive.org">wa</a>
 			<a onclick="window.location='http://foo.com'">click me</a>
 		</body>
-	</html>
-	`
-
-	resp := &http.Response{
-		Body: io.NopCloser(bytes.NewBufferString(body)),
-	}
-	newURL := &models.URL{Raw: "http://ex.com"}
-	newURL.SetResponse(resp)
-	err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir())
-	if err != nil {
-		t.Errorf("ProcessBody() error = %v", err)
-	}
-	item := models.NewItem("test", newURL, "")
+	</html>`
+	item := setupItem(html)
 
 	outlinks, err := HTMLOutlinks(item)
 	if err != nil {
@@ -49,28 +54,15 @@ func TestHTMLOutlinks(t *testing.T) {
 
 // Test <audio> and <video> src extraction
 func TestHTMLAssetsAudioVideo(t *testing.T) {
-	config.InitConfig()
-	audioVideoBody := `
+	html := `
 	<html>
-		<head></head>
 		<body>
 			<video src="http://f1.com"></video>
 			<p>test</p>
 			<audio src="http://f2.com"></audio>
 		</body>
-	</html>
-	`
-
-	resp := &http.Response{
-		Body: io.NopCloser(bytes.NewBufferString(audioVideoBody)),
-	}
-	newURL := &models.URL{Raw: "http://ex.com"}
-	newURL.SetResponse(resp)
-	err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir())
-	if err != nil {
-		t.Errorf("ProcessBody() error = %v", err)
-	}
-	item := models.NewItem("test", newURL, "")
+	</html>`
+	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
 	if err != nil {
@@ -83,10 +75,8 @@ func TestHTMLAssetsAudioVideo(t *testing.T) {
 
 // Test [data-item], [style], [data-preview] attribute extraction
 func TestHTMLAssetsAttributes(t *testing.T) {
-	config.InitConfig()
 	html := `
 	<html>
-		<head></head>
 		<body>
 		 <div style="background: url('http://something.com/data.jpg')"></div>
 	   <div data-preview="http://archive.org">...</div>
@@ -95,19 +85,8 @@ func TestHTMLAssetsAttributes(t *testing.T) {
     		Click here for details
 			</div>
 		</body>
-	</html>
-	`
-
-	resp := &http.Response{
-		Body: io.NopCloser(bytes.NewBufferString(html)),
-	}
-	newURL := &models.URL{Raw: "http://ex.com"}
-	newURL.SetResponse(resp)
-	err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir())
-	if err != nil {
-		t.Errorf("ProcessBody() error = %v", err)
-	}
-	item := models.NewItem("test", newURL, "")
+	</html>`
+	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
 	if err != nil {
@@ -119,7 +98,6 @@ func TestHTMLAssetsAttributes(t *testing.T) {
 }
 
 func TestHTMLAssetsMeta(t *testing.T) {
-	config.InitConfig()
 	html := `
 	<html>
 		<head>
@@ -133,22 +111,89 @@ func TestHTMLAssetsMeta(t *testing.T) {
 		<body>
 			experiment
 		</body>
-	</html>
-	`
-
-	resp := &http.Response{
-		Body: io.NopCloser(bytes.NewBufferString(html)),
-	}
-	newURL := &models.URL{Raw: "http://ex.com"}
-	newURL.SetResponse(resp)
-	err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir())
-	if err != nil {
-		t.Errorf("ProcessBody() error = %v", err)
-	}
-	item := models.NewItem("test", newURL, "")
+	</html>`
+	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
+	if err != nil {
+		t.Errorf("HTMLAssets error = %v", err)
+	}
 	if len(assets) != 2 {
 		t.Errorf("We couldn't extract all meta & link assets. %d", len(assets))
+	}
+}
+
+func TestSrcset(t *testing.T) {
+	html := `
+	<html>
+		<body>
+		<img srcset="http://ex.com/a.jpg 480w, http://ex.com/b.jpg 800w"
+		    sizes="(max-width: 600px) 480px, 800px"
+			src="http://ex.com/c.jpg" />
+		<picture>
+		<source media="(min-width: 0px) and (-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi)" sizes="95vw" srcset="https://example.com/5.jpg?w=460 460w, http://example.com/img/media/6/5.jpg 340w"/>
+		</picture>
+		</body>
+	</html>`
+	item := setupItem(html)
+	assets, err := HTMLAssets(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML assets %s", err)
+	}
+	if len(assets) != 5 {
+		t.Errorf("We couldn't extract all assets. Extracted %d instead of 3.", len(assets))
+	}
+	if assets[0].Raw != "http://ex.com/c.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[0].Raw)
+	}
+	if assets[1].Raw != "http://ex.com/a.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[1].Raw)
+	}
+	if assets[2].Raw != "http://ex.com/b.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[2].Raw)
+	}
+}
+
+func TestUpperCase(t *testing.T) {
+	html := `
+	<HTML>
+	   <BODY>
+	   <A HREF="https://a.com/a.html">text</A>
+	   </BODY>
+    </HTML>`
+	item := setupItem(html)
+	outlinks, err := HTMLOutlinks(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML outlinks %s", err)
+	}
+	if len(outlinks) != 1 {
+		t.Errorf("We couldn't extract all HTML outlinks. Extracted %d instead of 1", len(outlinks))
+	}
+}
+
+func TestCSS(t *testing.T) {
+	html := `<html>
+	<head>
+	  <style type="text/css">
+	  #head{
+	    background:transparent url(http://g.org/images/logo.jpg);
+   	  }
+	  #footer{
+		background-image:url(http://m.gr/genbg?a=2&amp;b=1);
+	  }
+	  @import 'http://foo.org/common.css';
+	  </style>
+    </head>
+	<body>
+	  <div style="background: url(http://n.ua/img/bg.png);">
+	</body>
+    </html>`
+	item := setupItem(html)
+	assets, err := HTMLAssets(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML assets %s", err)
+	}
+	if len(assets) != 3 {
+		t.Errorf("We couldn't extract all HTML assets. Extracted %d instead of 3", len(assets))
 	}
 }
