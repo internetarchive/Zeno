72af25c8d8b3178863215503853a8482912bbe44
vbanos <vbanos@gmail.com>
2025-05-23 21:21:26 +0200

    Add unit tests to cover more HTML assets/outlinks extraction

--- internal/pkg/postprocessor/extractor/html_test.go
+++ internal/pkg/postprocessor/extractor/html_test.go
@@ -23,8 +23,7 @@ func setupItem(html string) *models.Item {
 	}
 	newURL := &models.URL{Raw: "http://ex.com"}
 	newURL.SetResponse(resp)
-	err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir())
-	if err != nil {
+	if err := archiver.ProcessBody(newURL, false, false, 0, os.TempDir()); err != nil {
 		panic(err)
 	}
 	return models.NewItem("test", newURL, "")
@@ -41,8 +40,7 @@ func TestHTMLOutlinks(t *testing.T) {
 			<a href="https://web.archive.org">wa</a>
 			<a onclick="window.location='http://foo.com'">click me</a>
 		</body>
-	</html>
-	`
+	</html>`
 	item := setupItem(html)
 
 	outlinks, err := HTMLOutlinks(item)
@@ -63,8 +61,7 @@ func TestHTMLAssetsAudioVideo(t *testing.T) {
 			<p>test</p>
 			<audio src="http://f2.com"></audio>
 		</body>
-	</html>
-	`
+	</html>`
 	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
@@ -88,8 +85,7 @@ func TestHTMLAssetsAttributes(t *testing.T) {
     		Click here for details
 			</div>
 		</body>
-	</html>
-	`
+	</html>`
 	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
@@ -115,8 +111,7 @@ func TestHTMLAssetsMeta(t *testing.T) {
 		<body>
 			experiment
 		</body>
-	</html>
-	`
+	</html>`
 	item := setupItem(html)
 
 	assets, err := HTMLAssets(item)
@@ -125,5 +120,80 @@ func TestHTMLAssetsMeta(t *testing.T) {
 	}
 	if len(assets) != 2 {
 		t.Errorf("We couldn't extract all meta & link assets. %d", len(assets))
+	}
+}
+
+func TestSrcset(t *testing.T) {
+	html := `
+	<html>
+		<body>
+		<img srcset="http://ex.com/a.jpg 480w, http://ex.com/b.jpg 800w"
+		    sizes="(max-width: 600px) 480px, 800px"
+			src="http://ex.com/c.jpg" />
+		<picture>
+		<source media="(min-width: 0px) and (-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi)" sizes="95vw" srcset="https://example.com/5.jpg?w=460 460w, http://example.com/img/media/6/5.jpg 340w"/>
+		</picture>
+		</body>
+	</html>`
+	item := setupItem(html)
+	assets, err := HTMLAssets(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML assets %s", err)
+	}
+	if len(assets) != 5 {
+		t.Errorf("We couldn't extract all assets. Extracted %d instead of 3.", len(assets))
+	}
+	if assets[0].Raw != "http://ex.com/c.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[0].Raw)
+	}
+	if assets[1].Raw != "http://ex.com/a.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[1].Raw)
+	}
+	if assets[2].Raw != "http://ex.com/b.jpg" {
+		t.Errorf("Invalid img URL extracted %v", assets[2].Raw)
+	}
+}
+
+func TestUpperCase(t *testing.T) {
+	html := `
+	<HTML>
+	   <BODY>
+	   <A HREF="https://a.com/a.html">text</A>
+	   </BODY>
+    </HTML>`
+	item := setupItem(html)
+	outlinks, err := HTMLOutlinks(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML outlinks %s", err)
+	}
+	if len(outlinks) != 1 {
+		t.Errorf("We couldn't extract all HTML outlinks. Extracted %d instead of 1", len(outlinks))
+	}
+}
+
+func TestCSS(t *testing.T) {
+	html := `<html>
+	<head>
+	  <style type="text/css">
+	  #head{
+	    background:transparent url(http://g.org/images/logo.jpg);
+   	  }
+	  #footer{
+		background-image:url(http://m.gr/genbg?a=2&amp;b=1);
+	  }
+	  @import 'http://foo.org/common.css';
+	  </style>
+    </head>
+	<body>
+	  <div style="background: url(http://n.ua/img/bg.png);">
+	</body>
+    </html>`
+	item := setupItem(html)
+	assets, err := HTMLAssets(item)
+	if err != nil {
+		t.Errorf("Error extracting HTML assets %s", err)
+	}
+	if len(assets) != 3 {
+		t.Errorf("We couldn't extract all HTML assets. Extracted %d instead of 3", len(assets))
 	}
 }
