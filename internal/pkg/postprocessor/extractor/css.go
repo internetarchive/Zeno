c6335175c573910a94306865f375e2ddcd153889
vbanos <vbanos@gmail.com>
2025-06-18 12:54:33 +0200

    Fix css extractor IsCSS error

--- internal/pkg/postprocessor/extractor/css.go
+++ internal/pkg/postprocessor/extractor/css.go
@@ -327,7 +327,7 @@ func parseCSS(reader io.Reader, inline bool) (links []string, atImportLinks []st
 // https://html.spec.whatwg.org/multipage/links.html#link-type-stylesheet:process-the-linked-resource
 // According to the spec, we should only check the Content-Type header if the resource is came from a HTTP(S) request.
 func IsCSS(URL *models.URL) bool {
-	return isContentType(URL.GetResponse().Header.Get("Content-Type"), "text/css")
+	return URL.GetMIMEType().Is("text/css")
 }
 
 // ExtractFromStringCSS extracts URLs from a CSS content string.
