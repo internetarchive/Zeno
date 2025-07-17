2c85aa5bd93b3153d4cf4ebefc932bc841fe664f
Vangelis Banos <vbanos@gmail.com>
2025-07-17 20:36:27 +0300

    Move SetDocument(nil) to Item.Close method (#393)
    Placing `SetDocument(nil)` in the end of the method doesn't guarantee
    that it will be executed always. If the method returns early, it is
    skipped and memory isn't freed.
    
    `Item` has a `Close()` method. Conceptually its better to include memory
    cleanup in `Item.Close`.
    
    Also, method `postprocessItem` includes `defer item.Close()`.
    
    So, it makes sense to move `SetDocument(nil)` in there.

--- internal/pkg/postprocessor/item.go
+++ internal/pkg/postprocessor/item.go
@@ -170,9 +170,6 @@ func postprocessItem(item *models.Item) []*models.Item {
 		}
 	}
 
-	// Make sure the goquery document's memory can be freed
-	item.GetURL().SetDocument(nil)
-
 	if !item.HasChildren() && !item.HasRedirection() && item.GetStatus() != models.ItemFailed {
 		logger.Debug("item has no children, setting as completed", "item_id", item.GetShortID())
 		item.SetStatus(models.ItemCompleted)
