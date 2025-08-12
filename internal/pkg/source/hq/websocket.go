3e4dc3a4bdc0814c1d05896065c8cebd034d5578
yzqzss <yzqzss@yandex.com>
2025-08-12 23:29:18 +0800

    Make Zeno to be signalable by HQ via websocket

--- internal/pkg/source/hq/websocket.go
+++ internal/pkg/source/hq/websocket.go
@@ -1,8 +1,14 @@
 package hq
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"syscall"
 	"time"
 
+	"github.com/gobwas/ws/wsutil"
 	"github.com/internetarchive/Zeno/internal/pkg/config"
 	"github.com/internetarchive/Zeno/internal/pkg/log"
 	"github.com/internetarchive/Zeno/internal/pkg/utils"
@@ -21,6 +27,8 @@ func (s *HQ) websocket() {
 	identifyTicker := time.NewTicker(time.Second)
 	defer identifyTicker.Stop()
 
+	go s.listenMessages()
+
 	for {
 		select {
 		case <-s.ctx.Done():
@@ -32,6 +40,66 @@ func (s *HQ) websocket() {
 			<-identifyTicker.C
 		}
 	}
+}
+
+func (s *HQ) listenMessages() {
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		default:
+			msgs, err := wsutil.ReadServerMessage(*s.client.WebsocketConn, nil) // this is a blocking op
+			if err != nil {
+				logger.Error("error reading message from HQ websocket, retrying", "err", err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			for _, msg := range msgs {
+				dispatchMessageByType(bytes.TrimSpace(msg.Payload))
+			}
+		}
+	}
+}
+
+func dispatchMessageByType(msg []byte) error {
+	type msgType struct {
+		Type string `json:"type"`
+	}
+	var m msgType
+	if err := json.Unmarshal(msg, &m); err != nil {
+		return err
+	}
+
+	var err error
+
+	switch m.Type {
+	case "signal":
+		err = handleSignalMsg(msg)
+	default:
+		logger.Warn("unknown HQ websocket message type", "type", m.Type, "payload", string(msg))
+	}
+	return err
+}
+
+func handleSignalMsg(msg []byte) error {
+	type signalMsg struct {
+		Signal int `json:"signal"`
+	}
+	var m signalMsg
+
+	if err := json.Unmarshal(msg, &m); err != nil {
+		return err
+	}
+
+	logger.Warn("sending signal to process", "signal", m.Signal)
+
+	p, err1 := os.FindProcess(os.Getpid())
+	err2 := p.Signal(syscall.Signal(m.Signal))
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("error sending signal %d to process %d: %v, %v", m.Signal, os.Getpid(), err1, err2)
+	}
+
+	return nil
 }
 
 func (s *HQ) sendIdentify(logger *log.FieldedLogger) {
