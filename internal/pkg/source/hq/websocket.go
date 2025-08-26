495aa1846438f214158d2ea9347b2cd22185a868
yzqzss <30341059+yzqzss@users.noreply.github.com>
2025-08-27 07:35:33 +0800

    Make Zeno to be signalable by HQ via websocket (#429)
    * Make Zeno to be signalable by HQ via websocket
    
    * check the error returned by dispatchMessageByType
    
    * chore: WARN with the additional info
    
    * add readTimeout

--- internal/pkg/source/hq/websocket.go
+++ internal/pkg/source/hq/websocket.go
@@ -1,13 +1,22 @@
 package hq
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"syscall"
 	"time"
 
+	"github.com/gobwas/ws/wsutil"
 	"github.com/internetarchive/Zeno/internal/pkg/config"
 	"github.com/internetarchive/Zeno/internal/pkg/log"
 	"github.com/internetarchive/Zeno/internal/pkg/utils"
 	"github.com/internetarchive/gocrawlhq"
 )
+
+var unknownMsgTypeErr = errors.New("unknown HQ websocket message type")
 
 // websocket connects to HQ's websocket and listen for messages.
 // It also sends and "identify" message to the HQ to let it know that
@@ -21,6 +30,8 @@ func (s *HQ) websocket() {
 	identifyTicker := time.NewTicker(time.Second)
 	defer identifyTicker.Stop()
 
+	go s.listenMessages()
+
 	for {
 		select {
 		case <-s.ctx.Done():
@@ -32,6 +43,76 @@ func (s *HQ) websocket() {
 			<-identifyTicker.C
 		}
 	}
+}
+
+const readTimeout = 30 * time.Second
+
+func (s *HQ) listenMessages() {
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		default:
+			conn := *s.client.WebsocketConn
+			conn.SetReadDeadline(time.Now().Add(readTimeout))
+			msgs, err := wsutil.ReadServerMessage(conn, nil)
+			conn.SetReadDeadline(time.Time{}) // Reset read deadline
+			if err != nil {
+				logger.Error("error reading message from HQ websocket, retrying", "err", err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			for _, msg := range msgs {
+				mType, err := dispatchMessageByType(bytes.TrimSpace(msg.Payload))
+				if err != nil {
+					logger.Error("error dispatching message by type", "msg_type", mType, "err", err)
+					continue
+				}
+			}
+		}
+	}
+}
+
+func dispatchMessageByType(msg []byte) (string, error) {
+	type msgType struct {
+		Type string `json:"type"`
+	}
+
+	var m msgType
+	var err error
+
+	if err = json.Unmarshal(msg, &m); err != nil {
+		return "", err
+	}
+
+	switch m.Type {
+	case "signal":
+		err = handleSignalMsg(msg)
+	default:
+		err = unknownMsgTypeErr
+	}
+	return m.Type, err
+}
+
+func handleSignalMsg(msg []byte) error {
+	type signalMsg struct {
+		Signal int `json:"signal"`
+	}
+	var m signalMsg
+
+	if err := json.Unmarshal(msg, &m); err != nil {
+		return err
+	}
+
+	logger.Warn("sending signal to process", "signal", m.Signal, "pid", os.Getpid(), "payload", string(msg))
+
+	p, err1 := os.FindProcess(os.Getpid())
+	err2 := p.Signal(syscall.Signal(m.Signal))
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("error sending signal %d to process %d: %v, %v", m.Signal, os.Getpid(), err1, err2)
+	}
+
+	return nil
 }
 
 func (s *HQ) sendIdentify(logger *log.FieldedLogger) {
