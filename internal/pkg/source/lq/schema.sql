ae406ef737c543708131d201d637f1c8616a469a
Jake L <NGTmeaty@users.noreply.github.com>
2025-09-22 20:40:36 +0000

    add index for hops and timestamp. Should improve performance for new databases.

--- internal/pkg/source/lq/schema.sql
+++ internal/pkg/source/lq/schema.sql
@@ -8,3 +8,4 @@ CREATE TABLE IF NOT EXISTS urls (
 );
 CREATE UNIQUE INDEX IF NOT EXISTS urls_value ON urls (value); -- for deduplication
 CREATE INDEX IF NOT EXISTS urls_status ON urls (status); -- for queueing
+CREATE INDEX IF NOT EXISTS urls_hops_timestamp ON urls (hops ASC, timestamp ASC); -- for sorting by crawl depth and time
